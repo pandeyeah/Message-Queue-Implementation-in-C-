956f076255a484ec267c3cf7b8849b565a533614
Akash Pandey <111501356+pandeyeah@users.noreply.github.com>
2025-03-13 08:43:15 +0530

    Create Message.h

---
+++ Message.h
@@ -0,0 +1,13 @@
+#ifndef MESSAGE_H
+#define MESSAGE_H
+
+#include <string>
+
+// Message structure
+struct Message {
+    int messageType;
+    std::string payload;
+    // Add any other fields as needed
+};
+
+#endif // MESSAGE_H
